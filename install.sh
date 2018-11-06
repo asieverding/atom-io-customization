02c032289d6cb9e10522265f26e3721ebefad042
Andre Sieverding <webmaster@andre-sieverding.de>
2018-11-06 11:16:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Display welcome message and info
 echo "\nWelcome to automatic Atom.io installer with packages by Andre Sieverding\n"
 
-# Get absolute directory path of current dir
+# Get absolute directory path of current file
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # Change into ~/.atom directory
@@ -22,7 +22,7 @@ echo "\n"
 cd ${dir}/atom
 
 # Copy all configurations into current directory
-echo "Updating package-list...\n"
+echo "Copying files...\n"
 
 for file in ./*
 do
@@ -44,7 +44,7 @@ cd ${dir}/icon
 # Copy all configurations into current directory
 for file in ./*
 do
-	# Only if it is a .cson, .coffee, .txt, .json or .less file
+	# Only if it is a .png file
 	if [[ "$file" == ./*.png ]]
 	then
 		# Change into ~/.atom directory
@@ -56,7 +56,7 @@ do
 		fi
 
 		# Change into ~/.atom/touchbar-icons directory
-		cd touchbar-icons
+		cd ./touchbar-icons
 
 		relFile=$(echo $file | cut -c 3-)
 		echo "Copying: ${relFile} -> ~/.atom/touchbar-icons/${relFile}"
